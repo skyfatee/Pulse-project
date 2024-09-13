6ea05e3d096cbbe628bd7d361005453fa15948d3
Tamerlan <roninfortnite71@gmail.com>
2024-09-14 00:41:25 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -105,5 +105,37 @@ logging(6, 8); */
         $(this).on('click', function() {
             $('#order .modal_descr').text($('.catalog_item_subtitle').eq(i).text());
             $('.overlay, #order').fadeIn('slow');
-        });
-    });
+        })
+    
+    // Validate
+      function validateForms(form) {
+          $(form).validate({
+              rules: {
+                  name: {
+                      required: true,
+                      minlength: 2
+                  },
+                  phone: "required",
+                  email: {
+                      required: true,
+                      email: true
+                  }
+              },
+              messages: {
+                  name: {
+                      required: "Пожалуйста, введите свое имя",
+                      minlength: jQuery.validator.format("Введите {0} символа!")
+                  },
+                  phone: "Пожалуйста, введите свой номер телефона",
+                  email: {
+                      required: "Пожалуйста, введите свою почту",
+                      email: "Неправильно введен адрес почты"
+                  }
+              }
+          });
+      };
+
+      validateForms('#consultation-form');
+      validateForms('#consultation form');
+      validateForms('#order form');
+});
