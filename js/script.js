240e0a17f48694861394edaac6ebcec54471250b
Tamerlan <roninfortnite71@gmail.com>
2024-10-05 17:57:00 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -153,16 +153,31 @@ logging(6, 8); */
         });
         return false;
     });
+
+    $(document).ready(function () {
+        // Add smooth scrolling to all links
+        $("a").on('click', function (event) {
+
+            // Make sure this.hash has a value before overriding default behavior
+            if (this.hash !== "") {
+                // Prevent default anchor click behavior
+                event.preventDefault();
+
+                // Store hash
+                var hash = this.hash;
+
+                // Using jQuery's animate() method to add smooth page scroll
+                // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+                $('html, body').animate({
+                    scrollTop: $(hash).offset().top
+                }, 50, function () {
+
+                    // Add hash (#) toz URL when done scrolling (default click behavior)
+                    window.location.hash = hash;
+                });
+            } 
+        });
+        new WOW().init();
+    });
 });
 
-    /* // Smooth scroll and page up
-    $(window).scroll(function() {
-        if ($(this).scrollTop() > 1600) {
-            $('.pageup').fadeIn();
-        } else {
-            $('.pageup').fadeOut();
-        }
-    });
-    new WOW().init();
-});
- */
